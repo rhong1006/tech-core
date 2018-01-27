35479297e59b5cba6155d5b28fee6e6c4f6fbea6
Rina Hong <rhong1006@gmail.com>
2018-01-27 10:51:06 -0800

    Search by tag is working

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -5,8 +5,27 @@ class Organization < ApplicationRecord
   has_many :events
 
 
-  scope :search, -> (name) {
+  scope :search_by_name, -> (name) {
     where("name ILIKE  '%#{name}%'")
+  }
+
+  scope :search_by_tag, -> (tag_array) {
+    # puts "===========tag_array length"
+    # puts tag_array
+    # tag_array.each do |tag_id|
+    #   # Tagging.where tag_id:tag_id do |t|
+    #   #   Organization.where t.organization
+    #   # end
+    #   # where("name ILIKE  '%#{tag_name}%'")
+    #   Tagging.joins(
+    #     "INNER JOIN tag
+    #     WHERE taggings.id = tag_id
+    #     ON taggings.tag_id = tags.id"
+    #   )
+    #
+    # end
+# => Organization.joins(:some_table)
+    joins(:taggings).where("taggings.tag_id IN (:tags)", tags: tag_array)
   }
 
 end
