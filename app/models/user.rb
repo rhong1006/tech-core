d34f1e584a020d2636923a0551ee05caf5a5078f
chilidoggca <jack.chili.lee@gmail.com>
2018-01-26 19:39:36 -0800

    added bcrypt to user model, add user references to organization model, and seeded database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_one :organization
+
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180127014428) do
+ActiveRecord::Schema.define(version: 20180127033025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20180127014428) do
     t.boolean "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_organizations_on_user_id"
   end
 
   create_table "taggings", force: :cascade do |t|
@@ -67,6 +69,7 @@ ActiveRecord::Schema.define(version: 20180127014428) do
   end
 
   add_foreign_key "events", "organizations"
+  add_foreign_key "organizations", "users"
   add_foreign_key "taggings", "organizations"
   add_foreign_key "taggings", "tags"
 end
