906b9038bb167359876d70cb63834162dc5029e0
Rina Hong <rhong1006@gmail.com>
2018-01-27 11:29:48 -0800

    Search index (#10)
    * Added search_by form in index view, added search function to Organization model
    
    * Can search by company name
    
    * Add taggings seed data in seed file
    
    * Add chosen-rails for tag in organization index, organize seed file
    
    * Destroy all of tags, and taggings before seeding, and delete unnecessary file
    
    * Search by tag is working
    
    * deleting unnecessary lines
    
    * Modify seed file to resolove conflict
    
    * Added tags=Tag.all
    
    * Change application.css to scss
    
    * Comment out bootstrap in application.scss
    
    * Refactor application.scss
    
    * Separate search_form to partials
    
    * Resolve all merge conflicts on index.html.erb

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,4 +3,14 @@ class Organization < ApplicationRecord
   has_many :tags, through: :taggings
   has_many :taggings, dependent: :destroy
   has_many :events
+
+
+  scope :search_by_name, -> (name) {
+    where("name ILIKE  '%#{name}%'")
+  }
+
+  scope :search_by_tag, -> (tag_array) {
+    joins(:taggings).where("taggings.tag_id IN (:tags)", tags: tag_array)
+  }
+
 end
