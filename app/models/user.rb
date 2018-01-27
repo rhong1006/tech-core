e0af5987f7f9c9d70282a6f22882b00c5642969a
Rina Hong <rhong1006@gmail.com>
2018-01-27 12:42:06 -0800

    Remove gecode_by:address (#14)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ApplicationRecord
   has_one :organization
 
-
-  geocoded_by :address
-  after_validation :geocode
-
-
   has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
