f05f44de2dde3e7a07998f83a0d9982ae9ffd917
Shaun-Schwartz <33434280+Shaun-Schwartz@users.noreply.github.com>
2018-01-27 13:25:14 -0800

    Correct organization delete logic, add ability for admin to manage users (#15)

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,7 @@ class Organization < ApplicationRecord
   belongs_to :user #User who creates the organization || admin
   has_many :tags, through: :taggings
   has_many :taggings, dependent: :destroy
-  
+
   has_many :events, dependent: :destroy
 
 
@@ -13,5 +13,5 @@ class Organization < ApplicationRecord
   scope :search_by_tag, -> (tag_array) {
     joins(:taggings).where("taggings.tag_id IN (:tags)", tags: tag_array)
   }
-  
+
 end
