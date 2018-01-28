7d72d7b5984f7ff1d84772910b0594dd88d40fac
Rina Hong <rhong1006@gmail.com>
2018-01-28 13:39:42 -0800

    Organize events controller (#65)
    * Remove format json, add more comments to explaing what functions do
    
    * Add :create to authorize_user in before_action
    
    * Order by start_time ascending in events
    
    * Remove :create from before_action auth_user
    
    * Add validation for events column
    
    * Add error message feature on events edit page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,13 +4,11 @@ class EventsController < ApplicationController
   before_action :authorize_user!, only: [:edit, :update, :destroy]
 
   # GET /events
-  # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.all.order(start_time: :asc)
   end
 
   # GET /events/1
-  # GET /events/1.json
   def show
   end
 
@@ -29,52 +27,39 @@ class EventsController < ApplicationController
   end
 
   # POST /events
-  # POST /events.json
   def create
     @event = Event.new event_params
     @event.organization = current_user.organizations.first
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        # format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        # format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.save
+      redirect_to @event, notice: 'Event was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
   def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
+    if @event.update(event_params)
+      redirect_to @event, notice: 'Event was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /events/1
-  # DELETE /events/1.json
   def destroy
     @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to events_url, notice: 'Event was successfully destroyed.'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+  # Use callbacks to share common setup or constraints between actions.
   def set_event
     @event = Event.find params[:id]
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+  # Only permits parameters listed below
   def event_params
     params.require(:event).permit(:title, :description, :location, :start_time, :end_time)
   end
@@ -85,4 +70,3 @@ class EventsController < ApplicationController
       redirect_to home_path
     end
   end
-end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,26 @@
 class Event < ApplicationRecord
   belongs_to :organization
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+
+
+  validate :start_date_cannot_be_in_the_past
+  validate :end_date_cannot_be_before_start_date
+
+  def start_date_cannot_be_in_the_past
+    if start_time.present? && start_time < Date.today
+      errors.add(:start_time, "can't be in the past")
+    end
+  end
+
+  def end_date_cannot_be_before_start_date
+    if end_time.present? && end_time < start_time
+      errors.add(:end_time, "can't be before start time")
+    end
+  end
+
 end
