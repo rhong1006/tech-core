c6087373a18c889d69383f23c8c73a22f2fcf4a4
Wellington Cunha <wdcunha@gmail.com>
2018-01-28 16:10:42 -0800

    Origin/set restrictions password (#78)
    * add href to back-to-top button
    
    * validation in user model for password and warn message

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,30 @@ class User < ApplicationRecord
   has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_PASSWORD_REGEX = /(      # Start of group
+        (?:                        # Start of nonmatching group, 4 possible solutions
+          (?=.*[a-z])              # Must contain one lowercase character
+          (?=.*[A-Z])              # Must contain one uppercase character
+          (?=.*\W)                 # Must contain one non-word character or symbol
+        |                          # or...
+          (?=.*\d)                 # Must contain one digit from 0-9
+          (?=.*[A-Z])              # Must contain one uppercase character
+          (?=.*\W)                 # Must contain one non-word character or symbol
+        |                          # or...
+          (?=.*\d)                 # Must contain one digit from 0-9
+          (?=.*[a-z])              # Must contain one lowercase character
+          (?=.*\W)                 # Must contain one non-word character or symbol
+        |                          # or...
+          (?=.*\d)                 # Must contain one digit from 0-9
+          (?=.*[a-z])              # Must contain one lowercase character
+          (?=.*[A-Z])              # Must contain one uppercase character
+        )                          # End of nonmatching group with possible solutions
+        .*                         # Match anything with previous condition checking
+      )/x                          # End of group
 
   validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
+
+  validates :password, length: { minimum: 6 }, format: {with: VALID_PASSWORD_REGEX, message: "Password must contain 3 of the following: a lowercase letter, an uppercase letter, a digit, a non-word character or symbol!"}
 
   validates :first_name, :last_name, presence: true
 
