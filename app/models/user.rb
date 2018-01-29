fc57d4658a1e6e8bda12fe5ff95194480924d26e
Shaun Schwartz <me@shaunschwartz.com>
2018-01-28 16:18:46 -0800

    remove password special characters validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,10 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
+
   validates :email, presence: true, uniqueness: true, format: VALID_EMAIL_REGEX
+
+  validates :password, length: { minimum: 6 }
 
   validates :first_name, :last_name, presence: true
 
